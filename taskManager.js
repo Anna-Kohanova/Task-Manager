bbfa6b409d0a2fd31eab82fa72abbe412e107faa
Anna-Kohanova <anitacx88@gmail.com>
2016-03-15 00:38:24 +0300

    Initial commit

---
+++ taskManager.js
@@ -0,0 +1,19 @@
+$(function () {
+    $("#tabs-1").tabs();
+});
+
+$(function () {
+    $(".accordion")
+            .accordion({
+                header: "> div > h3"
+            })
+            .sortable({
+                axis: "y",
+                handle: "h3",
+                stop: function (event, ui) {
+                    ui.item.children("h3").triggerHandler("focusout");
+
+                    $(this).accordion("refresh");
+                }
+            });
+});
