574cbd97dbf032805a8ce8e6a60d33bec3117ed9
Anna-Kohanova <anitacx88@gmail.com>
2016-03-19 00:02:33 +0300

    Add function for hiding div

--- taskManager.js
+++ taskManager.js
@@ -12,8 +12,11 @@ $(function () {
                 handle: "h3",
                 stop: function (event, ui) {
                     ui.item.children("h3").triggerHandler("focusout");
-
                     $(this).accordion("refresh");
                 }
             });
+});
+
+$("button.showButton").click(function () {
+    $("div.add_box").fadeToggle();
 });
