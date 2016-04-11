b06e7ecf2fe186d81c89627a691742d0e43408e3
Anna-Kohanova <anitacx88@gmail.com>
2016-04-11 15:59:31 +0300

    Add event for adding new tasks in list

--- taskManager.js
+++ taskManager.js
@@ -1,7 +1,7 @@
 $(function () {
     $(".accordion")
             .accordion({
-				heightStyle: "content",
+                heightStyle: "content",
                 header: "> div > h3"
             })
             .sortable({
@@ -14,15 +14,61 @@ $(function () {
             });
 });
 
+$(document).ready(function () {
+    $("#inputTask").keyup(function (event) {
+        if (event.keyCode == 13) {
+            $("#plus").click();
+        }
+    })
+});
+
+$('.todo-list').sortable();
 $("#plus").click(function () {
+    var itemValue = $('.new-todo').val();
+    
+    var ul = document.getElementById('taskList');
+    var li = document.createElement("li");
+    li.className = "task";
+
+    var ch = document.createElement("input");
+    ch.className = "toggle";
+    ch.type = "checkbox";
+    ch.onclick = toggleChecked;
+
+    var label = document.createElement("label");
+    label.className = "taskText";
+    label.textContent = itemValue;
+    
+    var a = document.createElement("a");
+    a.id = "remove";
+    
+    var span = document.createElement("span");
+    span.className = "glyphicon glyphicon-remove";
+    
+    a.appendChild(span);
+
+    li.appendChild(ch);
+    li.appendChild(label);
+    li.appendChild(a);
+
+    $(ul).prepend(li);
+
+    $('.new-todo').val(null);
+    $('.new-todo').focus();
+});
+
+$("#remove").click(function () {
     
 });
 
-$(".toggle").click(function() {
+function toggleChecked() {
     if ($(this).is(':checked')) {
         $(this).siblings('label').css('text-decoration', 'line-through');
     }
     else {
         $(this).siblings('label').css('text-decoration', 'none');
     }
-});
+    $('.new-todo').focus();
+}
+
+$(".toggle").on('click', toggleChecked);
