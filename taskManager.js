65950f9688181443dcb76bcaf07ab1ffe985cac6
Anna-Kohanova <anitacx88@gmail.com>
2016-04-12 22:58:21 +0300

    Add saving in localStorage

--- taskManager.js
+++ taskManager.js
@@ -14,6 +14,8 @@ $(function () {
             });
 });
 
+$('.todo-list').sortable();
+
 $(document).ready(function () {
     $("#inputTask").keyup(function (event) {
         if (event.keyCode == 13) {
@@ -22,43 +24,74 @@ $(document).ready(function () {
     })
 });
 
-$('.todo-list').sortable();
+var tasks = [],
+        completedTasks = [];
+
+if (JSON.parse(localStorage.getItem('taskList')))
+    tasks = JSON.parse(localStorage.getItem('taskList'));
+else
+    localStorage.setItem("taskList", JSON.stringify(tasks));
+
+updateListView();
+
+function addToList(task) {
+
+    tasks.push({
+        name: task,
+        done: false
+    });
+
+    updateListView();
+
+    localStorage.setItem("taskList", JSON.stringify(tasks));
+}
+
 $("#plus").click(function () {
     var itemValue = $('.new-todo').val();
-    
-    var ul = document.getElementById('taskList');
-    var li = document.createElement("li");
-    li.className = "task";
 
-    var ch = document.createElement("input");
-    ch.className = "toggle";
-    ch.type = "checkbox";
-    ch.onclick = toggleChecked;
-
-    var label = document.createElement("label");
-    label.className = "taskText";
-    label.textContent = itemValue;
-    
-    var a = document.createElement("a");
-    a.id = "remove";
-    
-    var span = document.createElement("span");
-    span.className = "glyphicon glyphicon-remove";
-    
-    a.appendChild(span);
-
-    li.appendChild(ch);
-    li.appendChild(label);
-    li.appendChild(a);
-
-    $(ul).prepend(li);
+    addToList(itemValue);
 
     $('.new-todo').val(null);
     $('.new-todo').focus();
 });
 
+function updateListView() {
+
+    var ul = document.getElementById('taskList');
+    ul.innerHTML = '';
+
+    tasks.forEach(function (task) {
+        var li = document.createElement("li");
+        li.className = "task";
+
+        var ch = document.createElement("input");
+        ch.className = "toggle";
+        ch.type = "checkbox";
+        ch.onclick = toggleChecked;
+
+        var label = document.createElement("label");
+        label.className = "taskText";
+        label.textContent = task.name;
+
+        var a = document.createElement("a");
+        a.id = "remove";
+
+        var span = document.createElement("span");
+        span.className = "glyphicon glyphicon-remove";
+
+        a.appendChild(span);
+
+        li.appendChild(ch);
+        li.appendChild(label);
+        li.appendChild(a);
+
+        ul.insertBefore(li, ul.firstChild);
+    });
+
+}
+
 $("#remove").click(function () {
-    
+
 });
 
 function toggleChecked() {
