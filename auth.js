6fa884ba9a1ccfbcded8d3f41e01598dbdd9e676
Anna-Kohanova <anitacx88@gmail.com>
2016-04-23 00:08:27 +0300

    Ready work with the google drive

---
+++ auth.js
@@ -0,0 +1,211 @@
+var CLIENT_ID = '165219503046-uec4qg1cqbaok7fq754t4bcqbl90hj82.apps.googleusercontent.com';
+var SCOPES = 'https://www.googleapis.com/auth/drive';
+
+var metadata = {}; // file metadata for creating/updating
+var appData = {}; // data for saving
+var contentType = 'application/json';
+var fileDefault = {}; // default source file with data	
+
+
+/**
+ * Called when the client library is loaded to start the auth flow.
+ */
+
+function handleClientLoad() {
+    window.setTimeout(checkAuth, 1);
+}
+
+
+/**
+ * Check if the current user has authorized the application.
+ */
+function checkAuth() {
+    gapi.auth.authorize(
+            {'client_id': CLIENT_ID, 'scope': SCOPES, 'immediate': true},
+    handleAuthResult);
+}
+
+function check() {
+    alert("1");
+}
+
+/**
+ * Called when authorization server replies.
+ *
+ * @param {Object} authResult Authorization result.
+ */
+function handleAuthResult(authResult) {
+
+    var authButton = document.getElementById('authorizeButton');
+    // authButton.style.display = 'none';
+
+    if (authResult && !authResult.error) {
+        gapi.client.load('drive', 'v2', function () {
+            getFile();
+        });
+    } else {
+        authButton.style.display = 'block';
+    }
+}
+
+
+function saveData() {
+    metadata = getFileMetadata();
+    checkFileExists(metadata.title, saveDataHandle);
+}
+
+function saveDataHandle(fileExists) {
+    if (fileExists) {
+        console.log('File already exists...');
+        updateFile(fileExists);
+    } else {
+        console.log('Will create a new file...');
+        createFile();
+    }
+}
+
+
+function getFileMetadata() {
+    return {
+        'title': 'tasks',
+        'mimeType': contentType
+    }
+}
+
+function createFile() {
+    console.log('Creating a new file (' + metadata.title + ') without data:');
+            const boundary = '-------314159265358979323846264';
+            const delimiter = "\r\n--" + boundary + "\r\n";
+            const close_delim = "\r\n--" + boundary + "--";
+            var base64Data = btoa(appData);
+    var multipartRequestBody =
+            delimiter +
+            'Content-Type: application/json\r\n\r\n' +
+            JSON.stringify(metadata) +
+            delimiter +
+            'Content-Type: ' + contentType + '\r\n' +
+            'Content-Transfer-Encoding: base64\r\n' +
+            '\r\n' +
+            base64Data +
+            close_delim;
+    var request = gapi.client.request({
+        'path': '/upload/drive/v2/files',
+        'method': 'POST',
+        'params': {'uploadType': 'multipart'},
+        'headers': {
+            'Content-Type': 'multipart/mixed; boundary="' + boundary + '"'
+        },
+        'body': multipartRequestBody});
+
+    request.execute(function (res) {
+        console.log('New file created! Fileinfo:');
+        console.log(res);
+        fileDefault.id = res.id;
+        fileDefault.title = res.title;
+        updateFile(fileDefault.id);
+    });
+}
+
+
+function updateFile(fileId) {
+    if (!fileId) {
+        return;
+    }
+    console.log('Updating file (' + metadata.title + ') with data:');
+    console.log(document.getElementById('json').value);
+            const boundary = '-------314159265358979323846';
+            const delimiter = "\r\n--" + boundary + "\r\n";
+            const close_delim = "\r\n--" + boundary + "--";
+            var base64Data = btoa(document.getElementById('json').value);
+    var multipartRequestBody =
+            delimiter +
+            'Content-Type: application/json\r\n\r\n' +
+            JSON.stringify(metadata) +
+            delimiter +
+            'Content-Type: ' + contentType + '\r\n' +
+            'Content-Transfer-Encoding: base64\r\n' +
+            '\r\n' +
+            base64Data +
+            close_delim;
+    var request = gapi.client.request({
+        'path': '/upload/drive/v2/files/' + fileId,
+        'method': 'PUT',
+        'params': {'uploadType': 'multipart', 'alt': 'json'},
+        'headers': {
+            'Content-Type': 'multipart/mixed; boundary="' + boundary + '"'
+        },
+        'body': multipartRequestBody});
+    request.execute(function (res) {
+        console.log('File successfully updated!');
+    });
+}
+
+
+function findFile(fname) {
+
+    var req = gapi.client.drive.files.list({q: "title='tasks'"});
+
+    req.execute(function (r) {
+        console.log('We are loading data...');
+
+        if (r.items && r.items.length && r.items[0].id) {
+            var request = gapi.client.drive.files.get({
+                'fileId': r.items[0].id
+            });
+
+            request.execute(function (resp) {
+                downloadFile(resp);
+            });
+        }
+
+        else {
+            document.getElementById('json').value = '';
+            createFile();
+        }
+
+    });
+
+}
+
+function getFile() {
+    metadata = getFileMetadata();
+    findFile(metadata.title);
+}
+
+
+function downloadFile(file) {
+    if (file.downloadUrl) {
+        var accessToken = gapi.auth.getToken().access_token;
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', file.downloadUrl);
+        xhr.setRequestHeader('Authorization', 'Bearer ' + accessToken);
+        xhr.onload = function () {
+            console.log('Data downloaded:')
+            appData = xhr.responseText;
+            document.getElementById('json').value = appData;
+            console.log(appData);
+        };
+        xhr.onerror = function () {
+            console.log('XHR error!');
+        };
+        xhr.send();
+    } else {
+        console.log('Can\'t download...');
+    }
+}
+
+
+
+function checkFileExists(fname, callback) {
+
+    var req = gapi.client.drive.files.list({q: "title='tasks'"});
+
+    req.execute(function (r) {
+
+        if (r.items && r.items.length && r.items[0].id) {
+            callback(r.items[0].id);
+        } else {
+            callback();
+        }
+    });
+}
--- taskManager.js
+++ taskManager.js
@@ -6,105 +6,107 @@ $(document).ready(function () {
     })
 });
 
-var tasks = [],
-        completedTasks = [];
+var completedTasks = [];
 
-if (JSON.parse(localStorage.getItem('taskList')))
-    tasks = JSON.parse(localStorage.getItem('taskList'));
-else
-    localStorage.setItem("taskList", JSON.stringify(tasks));
-	//var tasks = JSON.parse(document.getElementById('json').value);
-	alert("2");
+setTimeout(function () {
 
+    var tasks = [];
 
-updateListView();
+    if (document.getElementById('json').value != '') {
+        var tasks = JSON.parse(document.getElementById('json').value);
+        updateListView();
+    }
 
 
 /////// ADD NEW TASK IN LIST TASKS, UPDATING HTML, SAVING IN LOCAL STORAGE ////////
-function addToList(task) {
-    tasks.push({
-        name: task,
-        done: false
-    });
+    function addToList(task) {
 
-    updateListView();
-    saveLocalList();
-}
+        tasks.push({
+            name: task,
+            done: false
+        });
 
-/////// DELETE FROM LIST TASKS, UPDATING HTML, SAVING IN LOCAL STORAGE /////
-function deleteFromList(e) {
-    tasks.splice(e.target.parentElement.id, 1);
-    saveLocalList();
-    updateListView();
-}
-
-/////// "PLUS" CLICK EVENT, CALL ADDTOLIST FUNCTION //////
-$("#plus").click(function () {
-    var itemValue = $('.new-todo').val();
-
-    if (itemValue !== '') {
-        addToList(itemValue);
+        updateListView();
     }
 
-    $('.new-todo').val(null);
-    $('.new-todo').focus();
-});
+/////// DELETE FROM LIST TASKS, UPDATING HTML, SAVING IN LOCAL STORAGE /////
+    function deleteFromList(e) {
+        tasks.splice(e.target.parentElement.id, 1);
+        updateListView();
+    }
+
+/////// "PLUS" CLICK EVENT, CALL ADDTOLIST FUNCTION //////
+    $("#plus").click(function () {
+        var itemValue = $('.new-todo').val();
+
+        if (itemValue !== '') {
+
+            addToList(itemValue);
+        }
+
+        $('.new-todo').val(null);
+        $('.new-todo').focus();
+    });
 
 /////// "REMOVE" CLICK EVENT, CALL "DELETEFROMLIST" FUNCTION ///////
-$("#remove").click(function () {
-    deleteFromList();
-});
+    $("#remove").click(function () {
+        deleteFromList();
+    });
 
 
 /////// UPDATING OF HTML //////
-function updateListView() {
-    var ul = document.getElementById('taskList');
-    ul.innerHTML = '';
+    function updateListView() {
 
-    tasks.forEach(function (task) {
-        var li = document.createElement("li");
-        li.className = "task";
-        li.id = tasks.indexOf(task);
+        var ul = document.getElementById('taskList');
+        ul.innerHTML = '';
 
-        var ch = document.createElement("input");
-        ch.className = "toggle";
-        ch.type = "checkbox";
-        ch.onclick = toggleChecked;
-        ch.id = tasks.indexOf(task);
+        if (tasks.length != 0) {
+            tasks.forEach(function (task) {
+                var li = document.createElement("li");
+                li.className = "task";
+                li.id = tasks.indexOf(task);
 
-        var label = document.createElement("label");
-        label.className = "taskText";
-        label.textContent = task.name;
+                var ch = document.createElement("input");
+                ch.className = "toggle";
+                ch.type = "checkbox";
+                ch.onclick = toggleChecked;
+                ch.id = tasks.indexOf(task);
 
-        var span = document.createElement("span");
-        span.className = "remove glyphicon glyphicon-remove";
-        span.onclick = deleteFromList;
+                var label = document.createElement("label");
+                label.className = "taskText";
+                label.textContent = task.name;
 
-        li.appendChild(ch);
-        li.appendChild(label);
-        li.appendChild(span);
+                var span = document.createElement("span");
+                span.className = "remove glyphicon glyphicon-remove";
+                span.onclick = deleteFromList;
 
-        ul.insertBefore(li, ul.firstChild);
+                li.appendChild(ch);
+                li.appendChild(label);
+                li.appendChild(span);
+
+                ul.insertBefore(li, ul.firstChild);
+                $('.new-todo').focus();
+
+                document.getElementById('json').value = JSON.stringify(tasks);
+            });
+        } else {
+            document.getElementById('json').value = '';
+        }
+
+        saveData();
+
+    }
+
+    function toggleChecked() {
+        if ($(this).is(':checked')) {
+            $(this).siblings('label').css('text-decoration', 'line-through');
+        }
+        else {
+            $(this).siblings('label').css('text-decoration', 'none');
+        }
         $('.new-todo').focus();
-
-
-        document.getElementById('json').value = JSON.stringify(tasks);
-    });
-}
-
-////// SAVING IN LOCAL STORAGE //////
-function saveLocalList() {
-    localStorage.setItem("taskList", JSON.stringify(tasks));
-}
-
-function toggleChecked() {
-    if ($(this).is(':checked')) {
-        $(this).siblings('label').css('text-decoration', 'line-through');
     }
-    else {
-        $(this).siblings('label').css('text-decoration', 'none');
-    }
-    $('.new-todo').focus();
-}
 
-$(".toggle").on('click', toggleChecked);
+    $(".toggle").on('click', toggleChecked);
+
+}, 8000);
